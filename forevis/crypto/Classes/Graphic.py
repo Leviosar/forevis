5d911a6b4459b035120c55acfdd5c05901ef478a
Djalma <djalmirinha@gmail.com>
2019-11-28 22:42:19 -0300

    Add proper handling to average calculus

--- forevis/crypto/Classes/Graphic.py
+++ forevis/crypto/Classes/Graphic.py
@@ -2,13 +2,13 @@ from .Stoncks import Stoncks
 from random import random
 
 class Graphic:
-    def __init__(self, crypto, exchange, analysis):
+    def __init__(self, crypto, exchange, trade_type):
         self.crypto = crypto
         self.exchange = exchange
-        self.analysis = analysis
+        self.trade_type = trade_type
 
     def get_data(self):
-        request = Stoncks(function="DIGITAL_CURRENCY_DAILY", crypto=self.crypto, base_currency=self.exchange, analysis=self.analysis)
+        request = Stoncks(function="DIGITAL_CURRENCY_DAILY", crypto=self.crypto, base_currency=self.exchange, trade_type=self.trade_type)
         response = request.send()
         
         quotes = [
@@ -29,18 +29,9 @@ class Graphic:
         sell = []
 
         for index, value in enumerate(response["close"]):
-            if index >= 5:
-                media5.append(sum(response["close"][index-5:index]) / 5)
-            else:
-                media5.append(response["close"][index] + (random() * response["close"][index] * 0.025))
-            if index >= 10:
-                media10.append(sum(response["close"][index-10:index]) / 10)
-            else:
-                media10.append(response["close"][index] + (random() * response["close"][index] * 0.025))
-            if index >= 20:
-                media20.append(sum(response["close"][index-20:index]) / 20)
-            else:
-                media20.append(response["close"][index] + (random() * response["close"][index] * 0.025))
+            media5.append(sum(response["close"][index-5:index]) / 5)
+            media10.append(sum(response["close"][index-10:index]) / 10)
+            media20.append(sum(response["close"][index-20:index]) / 20)
 
             if media5[index - 1] < media10[index - 1] and media5[index] >= media10[index]:
                 buy.append(1)
@@ -55,4 +46,8 @@ class Graphic:
         volume = [response["volume"][x] for x in range(len(response["datetime"]))]
         market = [response["market_cap"][x] for x in range(len(response["datetime"]))]
         time = response["datetime"]
-        return quotes, volume, time, market, media5, media10, media20, buy, sell
+        
+        if self.trade_type == 'lt':
+            return quotes[10:], volume[10:], time[10:], market[10:], media5[10:], media10[10:], media20[10:], buy[10:], sell[10:]
+        else:
+            return quotes[10:40], volume[10:40], time[10:40], market[10:40], media5[10:40], media10[10:40], media20[10:40], buy[10:40], sell[10:40]
